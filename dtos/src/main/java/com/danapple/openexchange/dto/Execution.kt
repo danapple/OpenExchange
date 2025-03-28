58a4b212a50c1d939c82d7f97db4405b0774c253
Daniel I. Applebaum <danapple@danapple.com>
2025-03-28 09:50:22 +0100

    Add createTime to Execution

--- dtos/src/main/java/com/danapple/openexchange/dto/Execution.kt
+++ dtos/src/main/java/com/danapple/openexchange/dto/Execution.kt
@@ -4,6 +4,7 @@ import java.math.BigDecimal
 
 data class Execution (
     val clientOrderId: String,
-    val quantity: Int,         // Quantity of the trade (positive or negative)
-    val price: BigDecimal      // Price at which the trade was executed
+    val createTime: Long,
+    val price: BigDecimal,
+    val quantity: Int
 )
