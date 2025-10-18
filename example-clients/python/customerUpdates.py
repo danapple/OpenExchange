a097222f0ad9582cddde8f297beee1872183ec2c
Daniel I. Applebaum <danapple@danapple.com>
2025-10-18 13:34:46 +0100

    Now the subscribing websocket user only gets messages intended for their own user, and not any messages for other users.

--- example-clients/python/customerUpdates.py
+++ example-clients/python/customerUpdates.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3.12
+import getopt
 import stomper
+import sys
 import websocket
-websocket.enableTrace(True)
+
 
 def on_message(_, message):
     # print(message)
@@ -13,13 +15,30 @@ def on_message(_, message):
 def on_open(ws):
     ws.send("CONNECT\naccept-version:1.0,1.1,2.0\n\n\x00\n")
 
-    sub = stomper.subscribe("/topics/executions/BrokerA", 1, ack='auto')
+    sub = stomper.subscribe("/user/queue/executions", 1, ack='auto')
     ws.send(sub)
 
-ws_app = websocket.WebSocketApp("ws://localhost:5213/ws", cookie = 'customerKey=BrokerA', on_open=on_open, on_message=on_message)
 
-ws_app.run_forever(reconnect=1)
+def main(argv):
+    customerKey=''
+    try:
+        opts, args = getopt.getopt(argv, "", ["customerKey="])
+    except getopt.GetoptError:
+        print ('oops')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '--customerKey':
+            customerKey=arg
 
+    websocket.enableTrace(True)
+
+    ws_app = websocket.WebSocketApp("ws://localhost:5213/ws", cookie = 'customerKey=' + customerKey, on_open=on_open, on_message=on_message)
+
+    ws_app.run_forever(reconnect=1)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
 
 # ws.connect("ws://localhost:5213/ws")
